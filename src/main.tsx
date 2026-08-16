4b97822d689d1fa81134fd93c63d7c08812b5e59
sanjula26 <141200564+sanjula26@users.noreply.github.com>
2026-08-16 14:32:21 +0530

    Enable offline service worker and sync lifecycle

--- src/main.tsx
+++ src/main.tsx
@@ -2,10 +2,15 @@ import { StrictMode, useEffect } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.tsx'
+import { registerServiceWorker } from './lib/offline'
 import { startSyncManager } from './lib/syncManager'
 
 function AppBootstrap() {
-  useEffect(() => startSyncManager(), [])
+  useEffect(() => {
+    void registerServiceWorker()
+    return startSyncManager()
+  }, [])
+
   return <App />
 }
 
