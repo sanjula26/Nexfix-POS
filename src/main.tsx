015fccfce9cdecdb2d0bcd81c4fbf7d92e6e4147
sanjula26 <141200564+sanjula26@users.noreply.github.com>
2026-08-16 08:29:55 +0530

    Start offline sync manager with app lifecycle

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,16 @@
-import { StrictMode } from 'react'
+import { StrictMode, useEffect } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.tsx'
+import { startSyncManager } from './lib/syncManager'
+
+function AppBootstrap() {
+  useEffect(() => startSyncManager(), [])
+  return <App />
+}
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <App />
+    <AppBootstrap />
   </StrictMode>,
 )
