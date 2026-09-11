e903d073d34af11f2f9549a2621309c617c87145
sanjula26 <141200564+sanjula26@users.noreply.github.com>
2026-09-11 13:42:30 +0530

    chore: align migration history with production

--- supabase/migrations/20260911133500_harden_security_definer_search_paths.sql
+++
@@ -1,7 +0,0 @@
--- Harden SECURITY DEFINER helpers against search_path hijacking.
--- All referenced application objects are schema-qualified in these functions.
-
-alter function private.is_shop_member(uuid) set search_path = '';
-alter function private.is_shop_admin(uuid) set search_path = '';
-alter function private.register_pos_device_impl(text,text) set search_path = '';
-alter function private.upsert_pos_snapshot_impl(text,text,bigint,jsonb) set search_path = '';
